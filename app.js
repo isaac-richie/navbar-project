1f41629dd9f1bbbb12f1de89a57683b66e1edef9
adeyemi isaac <isaacoulwaseun66@gmail.com>
2022-10-04 12:45:34 +0100

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const navslide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navlinks = document.querySelectorAll('.nav-links li');
+
+
+    burger.addEventListener('click', () => {
+        //toggle nav
+        nav.classList.toggle('nav-active');
+        
+        //animate links
+        navlinks.forEach((link, index) => {
+            if(link.style.animation) {
+             link.style.animation = ''
+            } else {
+             link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.4}s`;
+            }
+         });
+         //burger animation
+         burger.classList.toggle('toggle');
+    });
+  
+}
+
+navslide(); 
